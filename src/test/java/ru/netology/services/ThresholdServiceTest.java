61523a9ab580d9180fe35c9d7965223f116bb545
Vera Sorokina <vera.sorokina.2013@mail.ru>
2023-10-14 13:38:22 +0400

    Code

---
+++ src/test/java/ru/netology/services/ThresholdServiceTest.java
@@ -0,0 +1,18 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class ThresholdServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/threshold.csv")
+    public void testShouldWork(int expected, int income, int expenses, int threshold) {
+        ThresholdService service = new ThresholdService();
+
+        int actual = service.calcThreshold(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
